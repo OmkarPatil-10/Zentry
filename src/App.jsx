347366fcc9827f07122ff29136809cb65d40f45f
OmkarPatil-10 <omkarpatil9935@gmail.com>
2025-06-19 23:28:20 +0530

    SetUp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold font-zentry text-violet-300">
+        Welocme to Awwards
+      </h1>
+    </main>
+  );
+}
+
+export default App;
